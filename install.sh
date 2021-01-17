3391be7a7de309ae36028cfe8e29e829e434d8ba
Shane Reilly <shanereilly@protonmail.com>
2021-01-16 20:08:54 -0500

    install fix

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-ln -s `pwd`/bcd /usr/localbin/bcd
+ln -s `pwd`/bcd /usr/local/bin/bcd
+ln -s `pwd`/bin2dec /usr/local/bin/bin2dec
+ln -s `pwd`/dec2bin /usr/local/bin/dec2bin
 ln -s `pwd`/bitcmp /usr/local/bin/bitcmp
 ln -s `pwd`/xor /usr/local/bin/xor
